f872a87263ef86aca74deb67ca6b4328713008e2
Marcos Scorzoni <mlessa@gmail.com>
2018-11-18 10:28:37 +0000

    config rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :warehouses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :warehouses
+    end
+  end
 end
