23118e64de4b28ea8de411b834b25b8999ac06d8
Marcos Scorzoni <mlessa@gmail.com>
2018-11-18 11:40:44 +0000

    setup connection through server and client using http-proxy-middleware dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import WarehousesFilter from './components/WarehousesFilter';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello
+        <WarehousesFilter />
       </div>
     );
   }
---
+++ client/src/components/WarehousesFilter.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WarehousesFilter extends Component {
+  render() {
+    return (
+      <div>
+        Warehouses 3
+      </div>
+    );
+  }
+}
+
+export default WarehousesFilter;
