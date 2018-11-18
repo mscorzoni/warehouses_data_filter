a737e83f9fd92470e6970097de0983d55a57a565
Marcos Scorzoni <mlessa@gmail.com>
2018-11-18 10:56:01 +0000

    parse csv and store on db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_options = {col_sep: ',', headers: :first_row }
+filepath = 'db/data/warehouses.csv'
+
+CSV.foreach(filepath, csv_options) do |row|
+  Warehouse.create!(
+                    internal_id: row['id'],
+                    name: row['name'],
+                    location: row['location'],
+                    latitude: row['lat'].to_f,
+                    longitude: row['lon'].to_f,
+                    rating: row['rating'].to_f,
+                    temperature: row['temperature'],
+                    capacity_sq_ft: row['capacity_sq_feet'].to_i
+                    )
+end
+
+puts 'Parsed CSV to db'
